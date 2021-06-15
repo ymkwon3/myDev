eb5e8e8253141734086b2acb75ddb065915c129a
youngmin <ymkwon3@gnu.ac.kr>
2021-06-15 21:58:37 +0900

    google oauth

--- src/main/java/com/myDev/first/service/PostsService.java
+++ src/main/java/com/myDev/first/service/PostsService.java
@@ -1,5 +1,6 @@
 package com.myDev.first.service;
 
+import com.myDev.first.controller.dto.PostsListResponseDto;
 import com.myDev.first.controller.dto.PostsResponseDto;
 import com.myDev.first.controller.dto.PostsSaveRequestDto;
 import com.myDev.first.controller.dto.PostsUpdateRequestDto;
@@ -7,8 +8,10 @@ import com.myDev.first.domain.posts.Posts;
 import com.myDev.first.domain.posts.PostsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -28,9 +31,23 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
